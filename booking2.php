b4167f9e41f77656fcf27acd87bc4cadab04f2ae
Bobo <57078191+bo08042@users.noreply.github.com>
2020-08-12 09:54:33 +0800

    update sweetalert.js

--- booking2.php
+++ booking2.php
@@ -290,6 +290,7 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/sweetalert2.all.min.js"></script>
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/jquery-ui.min.js"></script>
     <script src="js/jquery.slicknav.js"></script>
--- booking_event2.php
+++ booking_event2.php
@@ -280,6 +280,7 @@ require_once("./php/sql.php");
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/sweetalert2.all.min.js"></script>
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/jquery-ui.min.js"></script>
     <script src="js/jquery.slicknav.js"></script>
